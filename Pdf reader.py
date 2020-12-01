b1251100a7a7f6845158e9394a18e1f0a31cee75
Adarsh Kumar <65236741+ak47adarsh01@users.noreply.github.com>
2020-12-01 18:13:23 +0530

    Add files via upload

---
+++ Pdf reader.py
@@ -0,0 +1,14 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import pyttsx3
+import PyPDF2
+book = open('BOOkName.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(7, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
